86c2a763e22cd498467d1bb7abf835532702e463
cian.blake11 <cian.blake11@gmail.com>
2023-08-21 13:22:48 +0300

    images for all projects

--- src/components/projects/constant.js
+++ src/components/projects/constant.js
@@ -9,30 +9,30 @@ export const projects = [
         'title': 'Facial Recognition Web App',
         'description': 'Recreational web application to recognise any face uploaded to it.',
         'skills': ['ReactJ', 'JavaScript', 'AWS', 'AWS Rekognition', 'Python'],
-        'images': []
+        'images': ['/assets/face_1.png']
     },
     {
         'title': 'Dismantly Web App',
         'description': 'Modern operating system for auto dismantlers to optimise and digitalise the global used auto parts supply chain.',
         'skills': ['ReactJ', 'JavaScript', 'TypeScript', 'REDUX', 'AWS', 'Python'],
-        'images': []
+        'images': ['/assets/dismantly_web_1.png']
     },
     {
         'title': 'Dismantly Mobile App',
         'description': 'Manage your inventory and business on the go.',
         'skills': ['Flutter', 'Dart'],
-        'images': []
+        'images': ['/assets/dismantly_mobile_1.png']
     },
     {
         'title': 'Asterix Web App',
         'description': 'Educational web application to help teachers save time with test assigning and grading.',
         'skills': ['ReactJ', 'JavaScript', 'TypeScript', 'REDUX', 'AWS', 'Python'],
-        'images': []
+        'images': ['/assets/asterix_1.png']
     },
     {
         'title': 'Xanta Website',
         'description': 'Website for gift giving and receiving.',
         'skills': ['ReactJ', 'JavaScript'],
-        'images': []
+        'images': ['/assets/xanta_1.png']
     },
 ]
