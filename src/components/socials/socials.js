c49b920b0550670cedad6411bd8f92045f1c2ea4
cian.blake11 <cian.blake11@gmail.com>
2023-08-21 16:02:56 +0300

    social links

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -9,22 +9,22 @@ function Socials() {
   return (
     <Grid container direction='row' >
         <Grid item>
-          <StyledIconButton>
+          <StyledIconButton href='https://www.linkedin.com/in/cian-blake/' target='_blank'>
             <LinkedInIcon />
           </StyledIconButton>
         </Grid>
         <Grid item>
-          <StyledIconButton>
+          <StyledIconButton href='https://github.com/cianblake' target='_blank'>
             <GitHubIcon />
           </StyledIconButton>
         </Grid>
         <Grid item>
-          <StyledIconButton>
-            <EmailIcon />
-          </StyledIconButton> 
+            <StyledIconButton href='mailto:dev@cianblake.com' target='_blank'>
+                <EmailIcon />
+            </StyledIconButton> 
         </Grid>
         <Grid item>
-          <StyledIconButton>
+          <StyledIconButton href='https://www.youtube.com/@cian_and_doggy_adventures/' target='_blank'>
             <YouTubeIcon />
           </StyledIconButton>  
         </Grid>
