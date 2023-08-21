c14b631de77063286dac04c4ab0a160dd9fa8aed
cian.blake11 <cian.blake11@gmail.com>
2023-08-21 14:12:31 +0300

    view pdf

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -3,17 +3,21 @@ import { Stack } from '@mui/material';
 import { StyledBodyTypography } from '../../pages/styled.js';
 import { experience } from './constant';
 import Listing from './listing/listing';
-import { StyledStack } from './styled';
+import { StyledStack, StyledButton } from './styled';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function Experience() {
   return (
-    <Stack>
+    <Stack display='flex' alignItems={'flex-start'}>
         <StyledBodyTypography isAlternateColor>Experience</StyledBodyTypography>
         <StyledStack>
           {experience.map((data, index) => (
               <Listing key={index} {...data} />
           ))}
         </StyledStack>
+        <a href="/assets/cian-blake-cv.pdf" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+          <StyledButton endIcon={<ArrowForwardIcon />}>See Full Resume</StyledButton>
+        </a>
     </Stack>
 
   );
--- src/components/experience/styled.js
+++ src/components/experience/styled.js
@@ -1,4 +1,4 @@
-import {Stack , Divider} from '@mui/material';
+import {Stack , Divider, Button} from '@mui/material';
 import styled from '@emotion/styled';
 
 export const DottedDivider = styled(Divider)({
@@ -7,4 +7,14 @@ export const DottedDivider = styled(Divider)({
 
 export const StyledStack = styled(Stack)({
     gap: '10px',
+});
+
+export const StyledButton = styled(Button)({
+    padding: '0 !important',
+    color: '#E2E8F0',
+    fontFamily: 'Courier',
+    fontSize: '0.75rem',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: '1.25rem',
 });
