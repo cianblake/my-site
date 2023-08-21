b463aaed71414577d405b012ea02d3198d2b4742
cian.blake11 <cian.blake11@gmail.com>
2023-08-21 16:21:58 +0300

    mouse effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import LandingPage from './pages/landingPage';
 import { StyledBox } from './styled';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useState, useEffect } from 'react';
 
 const theme = createTheme();
 
 function App() {
+  const [position, setPosition] = useState({ x: '50%', y: '50%' });
+
+  useEffect(() => {
+      const handleMouseMove = (e) => {
+          setPosition({ x: `${e.clientX}px`, y: `${e.clientY}px` });
+      };
+
+      window.addEventListener('mousemove', handleMouseMove);
+
+      return () => {
+          window.removeEventListener('mousemove', handleMouseMove);
+      };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-      <StyledBox>
+      <StyledBox style={{ '--x': position.x, '--y': position.y }}>
         <LandingPage />
       </StyledBox>
     </ThemeProvider>
--- src/styled.js
+++ src/styled.js
@@ -2,11 +2,23 @@ import { Box } from '@mui/material';
 import styled from '@emotion/styled';
 
 export const StyledBox = styled(Box)({
-  display: 'flex',
-  justifyContent: 'center',
-  alignContent: 'center',
-  padding: '100px 40px 0 40px',
-  maxHeight: 'calc(100vh - 100px)',
-  height: '100vh',
-  backgroundColor: '#000000',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: '100px 40px 0 40px',
+    maxHeight: 'calc(100vh - 100px)',
+    height: '100vh',
+    backgroundColor: '#000000',
+    overflow: 'hidden',
+    '&::before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'radial-gradient(circle 100px at var(--x) var(--y), transparent 0%, rgba(0, 0, 0, 0.7) 150px)',
+        pointerEvents: 'none',
+    },
 });
